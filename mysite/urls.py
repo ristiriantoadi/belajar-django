7141d96a395b370fd72fa0eaa6760b440c375ab7
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-02-28 18:40:43 +0800

    done learning about django return html / templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view,another_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view,name="home"),
+    path('another',another_view,name="another")
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view():
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request,*args, **kwargs):
+    print(request.user)
+    return render(request,"pages/home.html")
+def another_view(request,*args, **kwargs):
+    return render(request,"pages/another.html",{})
